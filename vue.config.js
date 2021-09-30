e2d29228597e62612a24226f56453a0f05d37e46
MohammedShaibani <sonngohun@hotmail.ca>
2021-09-30 15:09:13 -0400

    changed vue config

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: '/personal-site/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/personal-site/'
+    : '/'
 }
